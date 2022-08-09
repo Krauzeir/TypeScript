e15c2bb5d0fd1c281a031e0928d98f7ffa9b828c
Soluevo Mac Mini 04 <mm04@Mac-mini-2.local>
2022-08-09 10:37:53 -0300

    Typescript Day 2

--- curso-typescript/interfaces/interfaces.js
+++ curso-typescript/interfaces/interfaces.js
@@ -8,10 +8,35 @@ function mudarNome(pessoa) {
 }
 const pessoa = {
     nome: 'João',
-    idade: 27
+    idade: 27,
+    saudar(sobrenome) {
+        console.log('Olá, meu nome é ' + this.nome + ' ' + sobrenome);
+    }
 };
 saudarComOla(pessoa);
 mudarNome(pessoa);
 saudarComOla(pessoa);
+pessoa.saudar('Skywalker');
 // saudarComOla({nome: 'Jonas'})
 // saudarComOla({nome: 'Bruno', idade: 27, xuahshxuas: 'não sei o elemento' }) /* O elemento 'idade' não estando configurado na interface Humano dará erro */
+// Usando Classes...
+// Ao utilizar o 'implements' com uma interface, a classe precisa respeitar o contrato existente na interface criada para os elementos OBRIGATÓRIOS, mas podendo adicionar novos elementos, exemplo 'status'
+class Cliente {
+    constructor() {
+        this.nome = "";
+        this.status = true;
+    }
+    saudar(sobrenome) {
+        console.log('Olá, meu nome era ' + this.nome + ' ' + sobrenome + ' e isso é: ' + this.status);
+    }
+}
+const meuCliente = new Cliente();
+meuCliente.nome = 'Lucas';
+meuCliente.saudar('Salvador');
+let potencia;
+potencia = function (base, exp) {
+    // Math.pow(3, 10)
+    // 3 ** 10
+    return Math.pow(base, exp);
+};
+console.log(potencia(3, 10));
--- curso-typescript/interfaces/interfaces.ts
+++ curso-typescript/interfaces/interfaces.ts
@@ -17,14 +17,52 @@ function mudarNome(pessoa: Humano) {
     pessoa.nome = 'Joana'
 }
 
-const pessoa = {
+const pessoa: Humano = {
     nome: 'João',
-    idade: 27
+    idade: 27,
+    saudar(sobrenome: string) {
+        console.log('Olá, meu nome é ' + this.nome + ' ' + sobrenome)
+    }
 }
 
 saudarComOla(pessoa)
 mudarNome(pessoa)
 saudarComOla(pessoa)
 
+pessoa.saudar('Skywalker')
+
 // saudarComOla({nome: 'Jonas'})
 // saudarComOla({nome: 'Bruno', idade: 27, xuahshxuas: 'não sei o elemento' }) /* O elemento 'idade' não estando configurado na interface Humano dará erro */
+
+
+
+// Usando Classes...
+
+// Ao utilizar o 'implements' com uma interface, a classe precisa respeitar o contrato existente na interface criada para os elementos OBRIGATÓRIOS, mas podendo adicionar novos elementos, exemplo 'status'
+class Cliente implements Humano {
+    nome: string = ""
+    status: boolean = true
+    saudar(sobrenome: string) {
+        console.log('Olá, meu nome era ' + this.nome + ' ' + sobrenome + ' e isso é: ' + this.status)
+    }
+}
+
+const meuCliente = new Cliente()
+meuCliente.nome = 'Lucas'
+meuCliente.saudar('Salvador')
+
+
+// Interface Função
+interface FuncaoCalculo {
+    (a: number, b: number): number
+}
+
+let potencia: FuncaoCalculo
+
+potencia = function(base: number, exp: number): number {
+    // Math.pow(3, 10)
+    // 3 ** 10
+    return Math.pow(base,exp)
+}
+
+console.log(potencia(3, 10))
